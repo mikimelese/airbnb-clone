52dea6fa74eee2724199c2de6b0f198b2fbbb46a
mikyas melese <mikimelese@gmail.com>
2025-03-15 11:58:22 +0100

    initial commit

---
+++ app/components/Modals/Modal.tsx
@@ -0,0 +1,159 @@
+"use client";
+import { useCallback, useEffect, useState } from "react";
+import { IoMdClose } from "react-icons/io";
+import Button from "../Button";
+
+interface ModalProps {
+  isOpen?: boolean;
+  onClose: () => {};
+  onSubmit: () => {};
+  title?: string;
+  body?: React.ReactElement;
+  footer?: React.ReactElement;
+  actionLabel: string;
+  disabled?: boolean;
+  secondaryAction?: () => {};
+  secondaryLabel?: string;
+}
+
+const Modal: React.FC<ModalProps> = ({
+  isOpen,
+  onClose,
+  onSubmit,
+  title,
+  body,
+  footer,
+  actionLabel,
+  disabled,
+  secondaryAction,
+  secondaryLabel,
+}) => {
+  const [showModal, setShowModal] = useState(isOpen);
+
+  useEffect(() => {
+    setShowModal(isOpen);
+  }, [isOpen]);
+
+  const handleClose = useCallback(() => {
+    if (disabled) {
+      return;
+    }
+    setShowModal(false);
+    setTimeout(() => {
+      onClose();
+    }, 300);
+  }, [disabled, onClose]);
+
+  const handleSubmit = useCallback(() => {
+    if (disabled) {
+      return;
+    }
+    onSubmit();
+  }, [disabled, onSubmit]);
+
+  const handleSecondaryAction = useCallback(() => {
+    if (disabled || !secondaryAction) {
+      return;
+    }
+
+    secondaryAction;
+  }, [disabled, secondaryAction]);
+
+  if (!isOpen) {
+    return null;
+  }
+  return (
+    <>
+      <div className="fixed inset-0 z-50 flex items-center justify-center overflow-x-hidden overflow-y-auto bg-neutral-800/70 outline-none focus:outline-none">
+        <div className="lg:h-auto md:h-auto relative mx-auto my-6 h-full w-full md:w-4/6 lg:w-3/6 xl:w-2/5">
+          {/* CONTENT */}
+          <div
+            className={`translate h-full duration-300 ${showModal ? "translate-y-0" : "translate-y-full"} ${showModal ? "opacity-100" : "opacity-0"}`}
+          >
+            <div
+              className="
+                translate
+                h-full 
+                lg:h-auto
+                md:h-auto
+                border-0
+                rounded-lg
+                shadow-lg
+                relative
+                flex
+                flex-col
+                w-full
+                bg-white
+                outline-none
+                focus:outline-none"
+            >
+              {/* HEADER */}
+              <div
+                className="
+                  flex
+                  items-center
+                  p-6
+                  rounded-t-2xl
+                  justify-center
+                  relative
+                  border-b-[1px]
+                  "
+              >
+                <button
+                  onClick={onClose}
+                  className="
+                    p-1
+                    border-0
+                    hover:opacity-70
+                    transition
+                    absolute
+                    left-9"
+                >
+                  <IoMdClose size={18} />
+                </button>
+                <div className="text-lg font-semibold">{title}</div>
+              </div>
+              {/* BODY */}
+              <div
+                className="
+                  relative p-6 flex-auto"
+              >
+                {body}
+              </div>
+              {/* FOOTER */}
+              <div
+                className="
+                  flex flex-col gap-2 p-6"
+              >
+                <div
+                  className="
+                    flex
+                    flex-row
+                    items-center
+                    gap-4
+                    w-full
+                    "
+                >
+                  {secondaryAction && secondaryLabel && (
+                    <Button
+                      disabled={disabled}
+                      label={secondaryLabel}
+                      onClick={handleSecondaryAction}
+                    />
+                  )}
+                  <Button
+                    disabled={disabled}
+                    label={actionLabel}
+                    onClick={handleSubmit}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Modal;
---
+++ app/components/navbar/UserMenu.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { AiOutlineMenu } from "react-icons/ai";
+import Avatar from "../Avatar";
+import { useCallback, useState } from "react";
+import MenuItem from "./MenuItem";
+import useRegisterModal from "@/app/hooks/useRegisterModal";
+
+const UserMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const registerModal = useRegisterModal();
+
+  const toggleOpen = useCallback(() => {
+    setIsOpen((value) => !value);
+  }, []);
+
+  return (
+    <div className="relative">
+      <div className="flex flex-row items-center gap-3">
+        <div
+          onClick={() => {}}
+          className="hidden cursor-pointer rounded-full px-4 py-3 text-sm font-semibold transition hover:bg-neutral-100 md:block"
+        >
+          Airbnb your home
+        </div>
+        <div
+          onClick={toggleOpen}
+          className="border-nutral-200 flex cursor-pointer flex-row items-center gap-3 rounded-full border-[1px] p-4 transition hover:shadow-md md:px-2 md:py-1"
+        >
+          <AiOutlineMenu />
+          <div className="hidden md:block">
+            <Avatar />
+          </div>
+        </div>
+      </div>
+      {isOpen && (
+        <div className="absolute top-12 right-0 w-[40vw] overflow-hidden rounded-xl bg-white text-sm shadow-md md:w-3/4">
+          <div>
+            <>
+              <MenuItem onClick={() => {}} label="Login" />
+              <MenuItem onClick={registerModal.onOpen} label="Sign up" />
+            </>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default UserMenu;
